ec5f9a62798fe629b994b86b7ce3cc9ee524b3a7
Hsin Tang <tang0251@algonquinlive.com>
2024-05-28 15:28:57 -0400

    week 2

--- lib/main.dart
+++ lib/main.dart
@@ -7,10 +7,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Demo Home Page',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
@@ -29,92 +30,59 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  var _counter = 0.0;
-  var myFontSize = 30.0;
-  var isChecked = false;
-  late TextEditingController _controller;
+  String password = '';
+  var imageSource = 'images/login.png';
 
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = TextEditingController();
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  void _incrementCounter() {
-    setState(() {
-      if(_counter < 99.0) _counter++;
-        myFontSize = _counter;
-    });
-  }
-
-  void setNewValue(double value)
-  {
-    setState(() {
-      _counter = value;
-      myFontSize = value;
-    });
+  void _login() {
+    if (password == 'QWERTY123') {
+      setState(() {
+        imageSource = 'images/idea.png';
+      });
+    } else {
+      setState(() {
+        imageSource = 'images/stop.png';
+      });
+    }
   }
 
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor: Theme
+            .of(context)
+            .colorScheme
+            .inversePrimary,
         title: Text(widget.title),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-                'You have pushed the button this many times:',
-                style:TextStyle( color:Colors.blue, fontSize: myFontSize),
+          children: [
+            const TextField(
+              decoration: InputDecoration(labelText: 'Login name'),
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Password'),
+              obscureText: true,
+              onChanged: (value) {
+                password = value;
+              },
             ),
             ElevatedButton(
-                onPressed: () {
-                  var myText = _controller.value.text;
-                  _controller.text = "You typed:" + myText;
-                },
-                child:Image.asset("images/algonquin.jpg", width: 200, height:200) ),
-            Checkbox(value: isChecked, onChanged: (bool ? ch) {
-              if (ch != null) {
-                setState(() {
-                  isChecked = ch;
-                });
-              }
-            }),
-            Switch(value:isChecked, onChanged: (newValue) {
-              setState(() {
-                isChecked = newValue;
-              });
-            }),
-            TextField( controller: _controller,
-              decoration: InputDecoration(
-                  hintText:"Type here",
-                  border: OutlineInputBorder(),
-                  labelText: "First name"
-              )),
+              onPressed: _login,
+              child: Text('Login'),
+            ),
+            SizedBox(height: 20),
+            Image.asset(
+              imageSource,
+              width: 300,
+              height: 300,
+            ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
     );
   }
-}
-
-void buttonClicked(){
-
 }
